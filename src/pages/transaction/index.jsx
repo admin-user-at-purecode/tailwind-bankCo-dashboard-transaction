7f293daad9b9682ea3477ba2d533af26634bf66f
Manish Gupta <manish.info2020@gmail.com>
2024-03-08 12:05:08 +0530

    screenshots and variant added

--- src/pages/transaction/index.jsx
+++ src/pages/transaction/index.jsx
@@ -5,10 +5,12 @@ import Wallet from "../../component/wallet";
 function Transaction() {
   return (
     <main className="w-full xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
-      {/* write your code here */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:w-70 w-full 2xl:mb-0 mb-6">
           <ListTab pageSize={9} />
+          <div className="my-5">
+            <TeamChat />
+          </div>
         </section>
         <section className="2xl:flex-1 w-full">
           <Wallet />
