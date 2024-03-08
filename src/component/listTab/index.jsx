66e7546f41e17187acf0323c6575887466a4e14f
Manish Gupta <manish.info2020@gmail.com>
2024-03-08 15:14:44 +0530

    variant and screenshots added

--- src/component/listTab/index.jsx
+++ src/component/listTab/index.jsx
@@ -5,19 +5,19 @@ import Search from "../forms/Search";
 import UserTab from "./UserTab";
 
 function ListTab({ pageSize }) {
-    return (
-        <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
-            <div className="flex flex-col space-y-5">
-                <div className="flex h-[56px] w-full space-x-4">
-                    <Search />
-                    <Filter options={["January", "February", "March"]} />
-                </div>
-                <FilterFull />
-                <UserTab pageSize={pageSize} />
-                <Pagination />
-            </div>
+  return (
+    <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
+      <div className="flex flex-col space-y-5">
+        <FilterFull />
+        <div className="flex h-[56px] w-full space-x-4">
+          <Search />
+          <Filter options={["January", "February", "March"]} />
         </div>
-    );
+        <UserTab pageSize={pageSize} />
+        <Pagination />
+      </div>
+    </div>
+  );
 }
 
 export default ListTab;
